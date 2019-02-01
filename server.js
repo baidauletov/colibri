5558e384ab5e7b0f7286633937caf842bd974f57
Sultan <tochkaapi@mail.ru>
2019-02-01 18:17:20 +0600

    handlebars view added to project

--- server.js
+++ server.js
@@ -1,34 +1,42 @@
 const express = require('express');
+const handlebars = require('express-handlebars').create({ defaultLayout: 'main'});
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+// start handlebars engine
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
-// get method of pages
-// must be earlier than use
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
+
+var fortunes = [
+	'Рекам нужны истоки.',
+	'Не бойся неведомого.',
+	'Будь проще везде где это возможно'
+]
+
+// 1. get method, for pages,
+// must be earlier ,than use method
+// 2. handlebars used for render
 app.get('/', function(req, res){
-		res.type('text/plain');
-		res.status(200);
-		res.send('Colibri Travel');
+		res.render('home');
 });
 
 app.get('/about', function(req, res){
-		res.type('text/plain');
-		res.status(200);
-		res.send('About Colibri Travel');
-})
+		var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+		res.render('about', { fortune: randomFortune });
+});
 
 app.use(function(req, res) {
-			res.type('text/plain');
 			res.status(404);
-			res.send('404 - not found');
+			res.render('404');
 });
 
 app.use(function(err, req, res, next) {
 			console.error(err.stack);
 			res.type('text/plain');
-			res.status(500);
-			res.send('500 - server error');
+			res.render('500');
 });
 
 app.listen(app.get('port'), function(){
