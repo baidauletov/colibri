6d6ac7510f907f57adb67d5ea2f5046539c920a5
Sultan <tochkaapi@mail.ru>
2019-02-01 17:10:31 +0600

    two routes added home and about

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
+// get method of pages
+// must be earlier than use
+app.get('/', function(req, res){
+		res.type('text/plain');
+		res.status(200);
+		res.send('Colibri Travel');
+});
+
+app.get('/about', function(req, res){
+		res.type('text/plain');
+		res.status(200);
+		res.send('About Colibri Travel');
+})
+
 app.use(function(req, res) {
 			res.type('text/plain');
 			res.status(404);
@@ -19,5 +33,5 @@ app.use(function(err, req, res, next) {
 
 app.listen(app.get('port'), function(){
 								console.log('Express server started on http://localhost:' + app.get('port') 
-								+ 'press Ctrl+C to stop');
+								+ ', press Ctrl+C to stop...');
 });
